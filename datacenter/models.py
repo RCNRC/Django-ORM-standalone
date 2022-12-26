99904caa991034956bcaa171c06d31d5aa0c6512
Ilya <Dragoilya@yandex.ru>
2022-12-26 03:00:07 +0300

    Done

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,26 @@
 from django.db import models
+from django.utils.timezone import localtime
+
+
+def get_duration(visit):
+    return localtime() - localtime(visit.entered_at)
+
+
+def format_duration(duration):
+    if duration.total_seconds() <= 86399:
+        return f"{duration.seconds//3600}ч {duration.seconds//60%60}м"
+    else:
+        return f"{duration.total_seconds()//(24*3600)}д {duration.seconds//3600%24}ч {duration.seconds//60%60}м"
+
+
+def is_visit_long(visit, minutes=60):
+    if visit.leaved_at is not None:
+        duration = visit.leaved_at - visit.entered_at
+    else:
+        duration = get_duration(visit)
+    if duration.seconds//60 > minutes:
+        return True
+    return False
 
 
 class Passcard(models.Model):
--- main.py
+++ main.py
@@ -1,12 +1,45 @@
 import os
 
 import django
+from django.utils.timezone import localtime
+from datetime import timedelta
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
 from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Visit
+from datacenter.models import get_duration, format_duration, is_visit_long
+
 
 if __name__ == '__main__':
     # Программируем здесь
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    passcards = Passcard.objects.all()
+    active_passcards = Passcard.objects.filter(is_active=True)
+    print("Активных пропусков: ", len(active_passcards))
+
+    non_closed_visits = Visit.objects.filter(leaved_at__isnull=True)
+    print(non_closed_visits)
+    for non_closed_visit in non_closed_visits:
+        delta = localtime() - localtime(non_closed_visit.entered_at)
+        print(non_closed_visit.leaved_at==None)
+        print(f"{non_closed_visit.passcard.owner_name} зашёл в хранилище, время по Москве:\n"
+              f"{localtime(non_closed_visit.entered_at)}\n\n"
+              f"Находится в хранилище:\n"
+              f"{delta.seconds//3600%24}:{delta.seconds//60%60}:{delta.seconds%60}")
+
+    # print(Passcard.objects.filter(visit__entered_at__contains="")) # => Jennifer Martin
+    # print(Visit.objects.filter(passcard__owner_name="Jennifer Martin"))
+    visits = Visit.objects.filter(passcard__owner_name="Jennifer Martin")
+    suspicion_visits = []
+    for iters, visit in enumerate(visits):
+        if is_visit_long(visit=visit, minutes=10):
+            suspicion_visits.append(visit)
+    print(f"Визиты дольше 10 мин {suspicion_visits}")
+
+    suspicion_visits = []
+    for iters, visit in enumerate(visits):
+        if is_visit_long(visit=visit, minutes=1000):
+            suspicion_visits.append(visit)
+    print(f"Визиты дольше 1000 мин {suspicion_visits}")
